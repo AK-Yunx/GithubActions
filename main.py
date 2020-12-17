0a3156f314aff0eaf0a78e820e639f8162f7a51e
AK-Yunx <yun1185448859@qq.com>
2020-12-17 11:28:01 +0800

    创建文件1

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-
+import os
 
 if __name__ == '__main__':
     with open('newFile.txt', 'w') as f:
         f.write("Hello")
+        print(f.name)
+    print("文件呢？")
+    print(os.path.abspath(__file__))
